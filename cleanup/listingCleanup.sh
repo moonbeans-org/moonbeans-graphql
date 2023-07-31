e76069ace7bb57ffbe45844223efad24ba02e2b3
Daniel Mackenthun <danielmackenthun@Daniels-MacBook-Pro.local>
2023-07-30 20:38:11 -0400

    fix cleanup task

--- cleanup/listingCleanup.sh
+++ cleanup/listingCleanup.sh
@@ -5,7 +5,7 @@ then
 	printf "NOTHING DELETED %s.\n" $(date +%s) >> deletions.txt
 else
 	latestFile=`ls -t deletions/* | head -1`
-	PGPASSWORD=$DBPASS psql -h $DBHOST -U postgres -d moonbeanstwochainz -a -f $latestFile
+	PGPASSWORD=$DBPASS psql -h $DBHOST -U $DBUSER -d $DBNAME -a -f $latestFile
 	mv $latestFile old-$latestFile
 	printf "RAN DELETION SCRIPT %s.\n" $latestFile >> deletions.txt
 fi
